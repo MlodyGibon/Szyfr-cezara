f91e37290a5b27bf9985cd181812608935c72943
MlodyGibon <mumiax515@gmail.com>
2022-05-30 14:02:58 +0200

    Dodaj pliki projektów.

---
+++ Szyfr-cezara/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace szyfer
+
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            foreach (var c in Console.ReadLine())
+            {
+                Console.Write(Convert.ToChar((c - 'a' + 1) % ('z' - 'a' + 1) + 'a'));
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
